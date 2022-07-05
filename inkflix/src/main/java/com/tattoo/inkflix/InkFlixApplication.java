63b1f3f7bc94569978fbc7074aa747eb29f63e4d
saransh2907 <saransh2907@outlook.com>
2022-07-05 21:38:01 +0530

    initialStructure Shree Ganeshaya Namah

---
+++ inkflix/src/main/java/com/tattoo/inkflix/InkFlixApplication.java
@@ -0,0 +1,13 @@
+package com.tattoo.inkflix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InkFlixApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InkFlixApplication.class, args);
+	}
+
+}
---
+++ inkflix/src/test/java/com/tattoo/inkflix/InkFlixApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tattoo.inkflix;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InkFlixApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
